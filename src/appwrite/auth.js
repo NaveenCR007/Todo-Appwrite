92dd65219d551a42ce5ebdc92fc37df7669bc5ef
Naveen <naveenbs135@gmail.com>
2025-12-22 20:18:16 +0530

    Add auth from appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,70 @@
+import { Client, ID, Account } from "appwrite";
+import conf from "../conf/config";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appWriteUrl)
+            .setProject(conf.appWriteProjectId)
+
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({ name, email, password }) {
+        try {
+            const userAccount = await this.account.create({
+                userId: ID.unique(),
+                email: email,
+                password: password,
+                name: name
+            })
+
+            if (userAccount) {
+                return this.login({ email, password })
+            } else {
+                return userAccount
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            const session = await this.account.createEmailPasswordSession({
+                email: email,
+                password: password
+            })
+
+            console.log("Login done!");
+            return session;
+        } catch (error) {
+            console.log("Can't able to login");
+            throw error
+        }
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Can't able to logout");
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            await this.account.get()
+        } catch(error) {
+            console.log("No such user found");
+            return null;
+        }
+    }
+}
+
+const authService = new AuthService()
+export default authService;
--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { logout } from '../../store/authSlice'
 
 function LogoutBtn() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const dispath = useDispatch()
+
+    const handleLogout = () => {
+        authSli
+    }
+
+    return (
+        <div>
+            <button onClick={handleLogout}>
+
+            </button>
+        </div>
+    )
 }
 
 export default LogoutBtn
